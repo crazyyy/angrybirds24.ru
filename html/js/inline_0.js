df0ce773857b2efa8c7db0b80a0e7557f1418116
crazyyy <crazyyy@gmail.com>
2017-03-28 13:16:04 +0300

    add html

---
+++ html/js/inline_0.js
@@ -0,0 +1,3 @@
+
+					var bhittani_plugin_kksr_js = {"nonce":"052f8bde57","grs":true,"ajaxurl":"http:\/\/game-angrybirds.ru\/wp-admin\/admin-ajax.php","func":"kksr_ajax","msg":"\u041e\u0446\u0435\u043d\u0438\u0442\u044c","fuelspeed":400,"thankyou":"\u0421\u043f\u0430\u0441\u0438\u0431\u043e!","error_msg":"\u041e\u0448\u0438\u0431\u043a\u0430 =(","tooltip":"1","tooltips":[{"tip":"","color":"#ffffff"},{"tip":"","color":"#ffffff"},{"tip":"","color":"#ffffff"},{"tip":"","color":"#ffffff"},{"tip":"","color":"#ffffff"}]};
+				
